2d2c8dc2ef11be2816cc8d465d96a15f66c92861
GabrielPLeal <lealgabrielp@gmail.com>
2021-02-25 23:02:01 -0300

    Alterado a igualdade da linha 8 para restrita.

--- src/components/repeticao/TabelaProdutos.jsx
+++ src/components/repeticao/TabelaProdutos.jsx
@@ -5,7 +5,7 @@ import produtos from '../../data/produtos';
 export default props => {
     const linhas = produtos.map((produto, i) => {
         return (
-            <tr key={produto.id} className={i % 2 == 0 ? 'par' : 'Impar'}>
+            <tr key={produto.id} className={i % 2 ===  0 ? 'par' : 'Impar'}>
                 <td>{produto.id}</td>
                 <td>{produto.nome}</td>
                 <td>R$ {produto.preco.toFixed(2).replace('.', ',')}</td>
