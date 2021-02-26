f931edc4f5f36fb9e83dcd6d9869a9b4afb7be16
GabrielPLeal <lealgabrielp@gmail.com>
2021-02-26 00:05:01 -0300

    Aula 29 - Renderização Condicional

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import React from 'react';
 
+import UsuarioInfo from './components/condicional/UsuarioInfo'
 import ParOuImpar from './components/condicional/ParOuImpar'
 import TabelaProdutos from './components/repeticao/TabelaProdutos'
 import ListaAlunos from './components/repeticao/ListaAlunos'
@@ -21,6 +22,10 @@ export default _ => (
             
             <Card titulo="#08 - Renderização Condicional" color="#8B4513">
                 <ParOuImpar numero={21}></ParOuImpar>
+                <UsuarioInfo usuario={{ nome: 'Fernando' }} />
+                <UsuarioInfo usuario={{ email: 'fer@gmail.com' }} />
+                <UsuarioInfo usuario={{}} />
+                <UsuarioInfo />
             </Card>
 
             <Card titulo="#07 - Desafio Repetição" color="#6495ED">
