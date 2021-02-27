0359a141edaa2e61bd4fc93250ce15f50771c9de
GabrielPLeal <lealgabrielp@gmail.com>
2021-02-26 21:14:02 -0300

    Aula 32 - Componente com estado

--- src/components/comunicacao/IndiretaPai.jsx
+++ src/components/comunicacao/IndiretaPai.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import IndiretaFilho from './IndiretaFilho'
 
 export default props => {
-    let nome = ''
-    let idade = 0
-    let nerd = false
-    // nome, idade e nerd
-    function fornecerInformacoes(nomeParam, idadeParam, nerdParam) {
-        nome = nomeParam
-        idade = idadeParam
-        nerd = nerdParam
+    const [nome, setNome] = useState('?')
+    const [idade, setIdade] = useState(0)
+    const [nerd, setNerd] = useState(false)
+
+    function fornecerInformacoes(nome, idade, nerd) {
+        setNome(nome)
+        setIdade(idade)
+        setNerd(nerd)
     }
 
     return (
         <div>
             <div>
-                <span>{nome}</span>     
-                <span>{idade}</span>
+                <span>{nome} </span>     
+                <span><strong>{idade}</strong> </span>
                 <span>{nerd ? 'Verdadeiro' : 'Falso'}</span>
             </div>
             <IndiretaFilho quandoClicar={fornecerInformacoes}></IndiretaFilho>
